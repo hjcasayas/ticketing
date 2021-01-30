dbd736f4dfa724a54dcfe6476a94124d21c4adb6
hjcasayas <henlyjade.casayas@gmail.com>
2021-01-31 04:03:02 +0800

    added user schema and updated sign up route

--- auth/src/routes/auth/sign-up.route.ts
+++ auth/src/routes/auth/sign-up.route.ts
@@ -1,24 +1,36 @@
 import { Router, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { DatabaseConnectionError, RequestValidationError } from "../../errors";
+import { User } from "../../models/user.model";
 
 const router = Router();
 
 const validation = [
-    body('email').isEmail().withMessage('Email must be provided'),
-    body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
+  body("email").isEmail().withMessage("Email must be provided"),
+  body("password")
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage("Password must be between 4 and 20 characters"),
 ];
 
-router.post('/sign-up', validation, (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
-    const { email, password } = req.body;
+router.post("/sign-up", validation, async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
+  }
+  const { email, password } = req.body;
 
-    throw new DatabaseConnectionError();
+  const existingUser = await User.findOne({ email });
 
-    res.status(200).json({ email, password });
+  if (existingUser) {
+    return res.json({ message: 'User already exists', data: existingUser });
+  }
+
+  const user = User.build({ email, password });
+
+  await user.save();
+
+  return res.status(201).json({ user });
 });
 
 export { router as SignUpRouter };
